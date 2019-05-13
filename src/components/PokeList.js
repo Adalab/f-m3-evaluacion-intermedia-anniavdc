ac482e84ea3126644679cae25efe346e64eaf382
anniavdc <anva1189@gmail.com>
2019-05-13 18:13:03 +0200

    Fix Pokemon component

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,28 +1,23 @@
-import React from 'react';
-// import Pokemon from './Pokemon'
+import React from "react";
+import Pokemon from "./Pokemon";
 
 class PokeList extends React.Component {
-    render() {
-      const {pokeData} = this.props;
-      
-      return (
-        <ul className="poke__list">
-        {pokeData.map(item=>(
-           <li className="poke__card" key={item.id}>
-              <img className="card__image" src={item.url} alt=""/>
-              <h2 className="card__title">{item.name}</h2>
-              <ul className="card__plus-info">
-              {item.types.map((type, index)=>(
-                <li className="info__item" key={index}>
-                  {type}
-                </li>
-              ))}
-              </ul>    
-           </li>
+  render() {
+    const { pokeData } = this.props;
+
+    return (
+      <ul className="poke__list">
+        {pokeData.map(item => (
+          <Pokemon
+            key={item.id}
+            src={item.url}
+            title={item.name}
+            type={item.types}
+          />
         ))}
-        </ul>
-      );
-    }
+      </ul>
+    );
   }
+}
 
 export default PokeList;
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,23 +1,22 @@
 import React from "react";
-import "./App.css";
 
-
-class Pokemon extends React.Component{
-    render(){
-        return(
-            <li className="poke__card" key={item.id}>
-              <img className="card__image" src={item.url} alt=""/>
-              <h2 className="card__title">{item.name}</h2>
-              <ul className="card__plus-info">
-              {item.types.map((type, index)=>(
-                <li className="info__item" key={index}>
-                  {type}
-                </li>
-              ))}
-              </ul>    
-           </li>
-        )
-    }
+class Pokemon extends React.Component {
+  render() {
+    const { key, src, title, type } = this.props;
+    return (
+      <li className="poke__card" key={key}>
+        <img className="card__image" src={src} alt="" />
+        <h2 className="card__title">{title}</h2>
+        <ul className="card__plus-info">
+          {type.map((type, index) => (
+            <li className="info__item" key={index}>
+              {type}
+            </li>
+          ))}
+        </ul>
+      </li>
+    );
+  }
 }
 
 export default Pokemon;
