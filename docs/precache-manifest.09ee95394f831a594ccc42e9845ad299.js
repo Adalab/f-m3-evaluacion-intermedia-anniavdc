921222fa5ff2348b95c700bf3e460d95a8ea31a0
anniavdc <anva1189@gmail.com>
2019-05-19 16:07:27 +0200

    Add docs

---
+++ docs/precache-manifest.09ee95394f831a594ccc42e9845ad299.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f3fae5a87a08e6645eefbeecb7b6a4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a6eb551510cb2cb9977e",
+    "url": "./static/css/main.e4b15208.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "./static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "a6eb551510cb2cb9977e",
+    "url": "./static/js/main.8003bb54.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
